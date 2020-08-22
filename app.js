15336f33db20cfcc84d84b462d9daaec087d8a2d
VirtualWolf <virtualwolf@virtualwolf.org>
2020-08-22 13:31:02 +1000

    Moved each HTTP request into its own try/catch, added timeouts and default data if an endpoint can't be contacted.

--- app.js
+++ app.js
@@ -7,18 +7,37 @@ const app = express();
 app.use(cors());
 
 app.get('/', async(req, res) => {
-    const outdoor = await request.get(config.outdoor);
-    const indoor = await request.get(config.indoor);
-    const power = await request.get(config.power);
+    let outdoorData, indoorData, powerData;
+
+    try {
+        const { body } = await request.get(config.outdoor).timeout({response: 2000});
+        outdoorData = body;
+    } catch(err) {
+        outdoorData = { temperature: '—', humidity: '—' };
+    }
+
+    try {
+        const { body } = await request.get(config.indoor).timeout({response: 2000});
+        indoorData = body;
+    } catch (err) {
+        indoorData = { temperature: '—', humidity: '—' };
+    }
+
+    try {
+        const { body } = await request.get(config.power).timeout({response: 15000});
+        powerData = {
+            consumption: (body.consumption[0].wNow/1000).toFixed(2),
+            production: (body.production[0].wNow/1000).toFixed(2),
+        }
+    } catch (err) {
+        powerData = { consumption: '—', production: '—' }
+    }
 
     return res.json({
-        outdoor: outdoor.body,
-        indoor: indoor.body,
-        power: {
-            consumption: power.body.consumption[0].wNow,
-            production: power.body.production[0].wNow,
-        },
-    })
+        outdoor: outdoorData,
+        indoor: indoorData,
+        power: powerData,
+    });
 });
 
 const port = process.env.PORT || 3000;
